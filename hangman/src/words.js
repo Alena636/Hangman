2b99a5201dee264c714a4cf0f56176213ae3046e
Alena636 <alenahomich636@gmail.com>
2024-02-02 11:28:09 +0300

    feat: add words and hint

---
+++ hangman/src/words.js
@@ -0,0 +1,42 @@
+const wordList = [
+  {
+    word: "javascript",
+    hint: "A high-level programming language commonly used for web development"
+  },
+  {
+    word: "function",
+    hint: "A reusable block of code designed to perform a specific task in JavaScript"
+  },
+  {
+    word: "array",
+    hint: "An ordered collection of values, often used to store lists of data in JavaScript"
+  },
+  {
+    word: "callback",
+    hint: "A function passed as an argument to another function to be executed later in JavaScript"
+  },
+  {
+    word: "event",
+    hint: "An occurrence that can trigger the execution of JavaScript code"
+  },
+  {
+    word: "DOM",
+    hint: "A programming interface for web documents, representing the structure of a document as a tree in JavaScript"
+  },
+  {
+    word: "loop",
+    hint: "A control flow statement for iterating over a block of code multiple times in JavaScript"
+  },
+  {
+    word: "closure",
+    hint: "A function that has access to variables from its outer (enclosing) scope in JavaScript."
+  },
+  {
+    word: "promise",
+    hint: "An object representing the eventual completion or failure of an asynchronous operation in JavaScript"
+  },
+  {
+    word: "constructor",
+    hint: "A special method for creating and initializing objects in JavaScript"
+  }
+]
